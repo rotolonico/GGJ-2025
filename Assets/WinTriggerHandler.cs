1b5321fb3d526e387ec73ffece286348bb506a81
Domenico Rotolo <rotolonico@gmail.com>
2025-01-26 14:02:13 +0100

    Added win trigger

--- Assets/WinTriggerHandler.cs
+++ Assets/WinTriggerHandler.cs
@@ -6,7 +6,7 @@ public class WinTriggerHandler : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             SceneManager.LoadScene("BossFightTest");
         }
