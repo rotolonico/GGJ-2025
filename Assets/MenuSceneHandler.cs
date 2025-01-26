7a9aa88aac1aa8e1ff3f4297d9056d3a7ea9f81a
Domenico Rotolo <rotolonico@gmail.com>
2025-01-26 12:25:17 +0100

    pushed scenes

--- Assets/MenuSceneHandler.cs
+++ Assets/MenuSceneHandler.cs
@@ -1,11 +1,27 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuSceneHandler : MonoBehaviour
 {
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
     public void StartGame()
     {
         SceneManager.LoadScene("TubeGeneratorExample");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
     }
 
     public void QuitGame()
