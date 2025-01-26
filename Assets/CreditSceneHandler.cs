300d152c2c756a0ea1e97059d5295b3a7cfd7cd5
Domenico Rotolo <rotolonico@gmail.com>
2025-01-26 12:29:10 +0100

    Finalized menu and credits

---
+++ Assets/CreditSceneHandler.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditSceneHandler : MonoBehaviour
+{
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
--- Assets/MenuSceneHandler.cs
+++ Assets/MenuSceneHandler.cs
@@ -4,16 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class MenuSceneHandler : MonoBehaviour
 {
-    private void Start()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
-
-    public void MenuScene()
-    {
-        SceneManager.LoadScene("MainMenu");
-    }
-    
     public void StartGame()
     {
         SceneManager.LoadScene("TubeGeneratorExample");
